77d9b0b3a1432eab5f083c70edf618a41c969369
Zach Schneider <schneidmaster@gmail.com>
2014-11-26 15:39:34 -0600

    Fix bugs in objects

--- objects.js
+++ objects.js
@@ -13,6 +13,19 @@ function isArray(obj) {
   return Object.prototype.toString.call(obj) === '[object Array]'
 }
 
+function typeNames(typeArray) {
+  types = []
+  typeArray.forEach(function(type) {
+    if(typeof type.typeName == "undefined") {
+      types.push(type)
+    }
+    else {
+      types.push(type.typeName)
+    }
+  })
+  return types.join(", ")
+}
+
 function checkExists(d, k) {
   if(d[k] == null) {
     throw "Object " + k + " missing"
@@ -48,14 +61,7 @@ function checkType(d, k, typeArray, isOpt) {
     return d[k]
   }
   else {
-    types = ""
-    typeArray.forEach(function(type) {
-      if(types != "") {
-        types += ", "
-      }
-      types += type.typeName
-    })
-    throw "Object type " + typeof(v) + " does not match required " + types + " in field " + k
+    throw "Object type " + typeof(v) + " does not match required " + typeNames(typeArray) + " in field " + k
   }
 }
 
@@ -71,12 +77,13 @@ function checkArrayType(d, k, typeArray, isOpt) {
     typeArray = [typeArray]
   }
 
-  d[k].forEach(function(e) {
-    if(!validateType(e, type)) {
-      throw "Object type " + typeof(v) + " does not match required " + type.typeName + " in field " + k
+  d[k].forEach(function(e, index) {
+    if(!validateType(e, typeArray)) {
+      throw "Object type " + typeof(e) + " does not match required " + typeNames(typeArray) + " in field " + k
     }
   })
-  return a
+
+  return d[k]
 }
 
 exports.VarType = function(d) {
@@ -86,6 +93,7 @@ exports.VarType = function(d) {
   that.type = checkType(d, "type", STRING_TYPE, false)
   return that
 }
+exports.VarType.typeName = "VarType"
 varTypes = {
   INPUT: exports.VarType({type: "INPUT"}),
   OUTPUT: exports.VarType({type: "OUTPUT"}),
@@ -96,10 +104,10 @@ exports.VarTypes = varTypes
 exports.VarDataType = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.VarDataType.prototype)
-  that.typeName = "VarDataType"
   that.type = checkType(d, "type", STRING_TYPE, false)
   return that
 }
+exports.VarDataType.typeName = "VarDataType"
 varDataTypes = {}
 varDataTypes[c.REAL_VAR_TKN] = exports.VarDataType({type: c.REAL_VAR_TKN})
 exports.VarDataTypes = varDataTypes
@@ -107,21 +115,21 @@ exports.VarDataTypes = varDataTypes
 exports.Var = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Var.prototype)
-  that.typeName = "Var"
   that.name = checkType(d, "name", STRING_TYPE, false)
   that.type = checkType(d, "type", exports.VarType, false)
   that.dataType = checkType(d, "dataType", exports.VarDataType, false)
   that.value = checkExists(d, "value")
   return that
 }
+exports.Var.typeName = "Var"
 
 exports.VarBlock = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Var.prototype)
-  that.typeName = "VarBlock"
-  that.vars = checkArrayType(d, "vars", exports.VarType, false)
+  that.vars = checkArrayType(d, "vars", exports.Var, false)
   return that
 }
+exports.VarBlock.typeName = "VarBlock"
 
 /**
  * Membership Functions (all fall under MemFunc type)
@@ -129,17 +137,17 @@ exports.VarBlock = function(d) {
 exports.Point = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Point.prototype)
-  that.typeName = "Point"
   that.x = checkType(d, "x", [exports.Var, NUMBER_TYPE], false)
   that.y = checkType(d, "y", [exports.Var, NUMBER_TYPE], false)
   return that
 }
+exports.Point.typeName = "Point"
 
 exports.MemFunc = function() {
   var that = createObject(exports.MemFunc.prototype)
-  that.typeName = "MemFunc"
   return that
 }
+exports.MemFunc.typeName = "MemFunc"
 
 exports.Trian = function(d) {
   d = (typeof d === "undefined") ? {} : d
@@ -220,28 +228,28 @@ exports.MemFuncs = memFuncs
 exports.Term = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Term.prototype)
-  that.typeName = "Term"
   that.name = checkType(d, "name", STRING_TYPE, false)
   that.func = checkType(d, "func", exports.MemFunc, false)
   return that
 }
+exports.Term.typeName = "Term"
 
 exports.FuzzifyBlock = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.FuzzifyBlock.prototype)
-  that.typeName = "FuzzifyBlock"
   that.var = checkType(d, "var", exports.Var, false)
   that.terms = checkArrayType(d, "terms", exports.Term, false)
   return that
 }
+exports.FuzzifyBlock.typeName = "FuzzifyBlock"
 
 exports.DefuzzMethod = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.DefuzzMethod.prototype)
-  that.typeName = "DefuzzMethod"
   that.method = checkType(d, "method", STRING_TYPE, false)
   return that
 }
+exports.DefuzzMethod.typeName = "DefuzzMethod"
 defuzzMethods = {}
 defuzzMethods[c.COG_METHOD_TKN] = exports.DefuzzMethod({method: c.COG_METHOD_TKN})
 defuzzMethods[c.COGS_METHOD_TKN] = exports.DefuzzMethod({method: c.COGS_METHOD_TKN})
@@ -253,25 +261,24 @@ exports.DefuzzMethods = defuzzMethods
 exports.DefuzzDefVal = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.DefuzzDefVal.prototype)
-  that.typeName = "DefuzzDefVal"
   that.isNC = checkType(d, "isNC", BOOL_TYPE, false)
   that.value = checkType(d, "value", NUMBER_TYPE, false)
   return that
 }
+exports.DefuzzDefVal.typeName = "DefuzzDefVal"
 
 exports.DefuzzRange = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.DefuzzRange.prototype)
-  that.typeName = "DefuzzRange"
   that.min = checkType(d, "min", NUMBER_TYPE, false)
   that.max = checkType(d, "max", NUMBER_TYPE, false)
   return that
 }
+exports.DefuzzRange.typeName = "DefuzzRange"
 
 exports.DefuzzifyBlock = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.DefuzzifyBlock.prototype)
-  that.typeName = "DefuzzifyBlock"
   that.var = checkType(d, "var", exports.Var, false)
   that.terms = checkArrayType(d, "terms", exports.Term, false)
   that.defuzzMethod = checkType(d, "defuzzMethod", exports.DefuzzMethod, false)
@@ -279,14 +286,15 @@ exports.DefuzzifyBlock = function(d) {
   that.range = checkType(d, "range", exports.Range, true)
   return that
 }
+exports.DefuzzifyBlock.typeName = "DefuzzifyBlock"
 
 exports.Operator = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Operator.prototype)
-  that.typeName = "Operator"
   that.operator = checkType(d, "operator", STRING_TYPE, false)
   return that
 }
+exports.Operator.typeName = "Operator"
 operators = {}
 operators[c.OPERATOR_DEF_AND_TKN] = exports.Operator({operator: c.OPERATOR_DEF_AND_TKN})
 operators[c.OPERATOR_DEF_OR_TKN] = exports.Operator({operator: c.OPERATOR_DEF_OR_TKN})
@@ -295,10 +303,10 @@ exports.Operators = operators
 exports.OperatorFunc = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.OperatorFunc.prototype)
-  that.typeName = "OperatorFunc"
   that.func = checkType(d, "func", STRING_TYPE, false)
   return that
 }
+exports.OperatorFunc.typeName = "OperatorFunc"
 operatorFuncs = {}
 operatorFuncs[c.AND_METHOD_BDIF_TKN] = exports.OperatorFunc({func: c.AND_METHOD_BDIF_TKN})
 operatorFuncs[c.AND_METHOD_MIN_TKN] = exports.OperatorFunc({func: c.AND_METHOD_MIN_TKN})
@@ -311,19 +319,19 @@ exports.OperatorFuncs = operatorFuncs
 exports.OperatorDef = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.OperatorFunc.prototype)
-  that.typeName = "OperatorDef"
   that.operator = checkType(d, "operator", exports.Operator, false)
   that.func = checkType(d, "func", exports.OperatorFunc, false)
   return that
 }
+exports.OperatorDef.typeName = "OperatorDef"
 
 exports.ActivationMethod = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.ActivationMethod.prototype)
-  that.typeName = "ActivationMethod"
   that.method = checkType(d, "method", STRING_TYPE, false)
   return that
 }
+exports.ActivationMethod.typeName = "ActivationMethod"
 activationMethods = {}
 activationMethods[c.ACT_METHOD_MIN_TKN] = exports.ActivationMethod({method: c.ACT_METHOD_MIN_TKN})
 activationMethods[c.ACT_METHOD_PROD_TKN] = exports.ActivationMethod({method: c.ACT_METHOD_PROD_TKN})
@@ -332,10 +340,10 @@ exports.ActivationMethods = activationMethods
 exports.AccumulationMethod = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.AccumulationMethod.prototype)
-  that.typeName = "AccumulationMethod"
   that.method = checkType(d, "method", STRING_TYPE, false)
   return that
 }
+exports.AccumulationMethod.typeName = "AccumulationMethod"
 accumulationMethods = {}
 accumulationMethods[c.ACCUM_METHOD_MAX_TKN] = exports.AccumulationMethod({method: c.ACCUM_METHOD_MAX_TKN})
 accumulationMethods[c.ACCUM_METHOD_BSUM_TKN] = exports.AccumulationMethod({method: c.ACCUM_METHOD_BSUM_TKN})
@@ -345,57 +353,57 @@ exports.AccumulationMethods = accumulationMethods
 exports.Assertion = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Assertion.prototype)
-  that.typeName = "Assertion"
   that.var = checkType(d, "var", exports.Var, false)
   that.value = checkExists(d, "val")
   that.not = checkType(d, "not", BOOL_TYPE, true)
   return that
 }
+exports.Assertion.typeName = "Assertion"
 
 exports.Expression = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Expression.prototype)
-  that.typeName = "Expression"
   that.operator = checkType(d, "operator", exports.Operator, false)
   that.firstHalf = checkType(d, "firstHalf", [exports.Assertion, exports.Expression], false)
   that.secondHalf = checkType(d, "secondHalf", [exports.Assertion, exports.Expression], false)
   return that
 }
+exports.Expression.typeName = "Expression"
 
 exports.WithCond = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.WithCond.prototype)
-  that.typeName = "WithCond"
   that.value = checkType(d, "value", [exports.Var, NUMBER_TYPE], false)
   return that
 }
+exports.WithCond.typeName = "WithCond"
 
 exports.Rule = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.Rule.prototype)
-  that.typeName = "Rule"
   that.ifCond = checkType(d, "ifCond", [exports.Assertion, exports.Expression], false)
   that.thenCond = checkType(d, "thenCond", [exports.Assertion, exports.Expression], false)
   that.withCond = checkType(d, "withCond", exports.WithCond, true)
   return that
 }
+exports.Rule.typeName = "Rule"
 
 exports.RuleBlock = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.RuleBlock.prototype)
-  that.typeName = "RuleBlock"
   that.name = checkType(d, "name", STRING_TYPE, false)
   that.rules = checkArrayType(d, "rules", exports.Rule, false)
   return that
 }
+exports.RuleBlock.typeName = "RuleBlock"
 
 exports.FunctionBlock = function(d) {
   d = (typeof d === "undefined") ? {} : d
   var that = createObject(exports.FunctionBlock.prototype)
-  that.typeName = "FunctionBlock"
   that.varBlocks = checkArrayType(d, "varBlocks", exports.VarBlock, false)
   that.fuzzifyBlocks = checkArrayType(d, "fuzzifyBlocks", exports.FuzzifyBlock, false)
   that.defuzzifyBlocks = checkArrayType(d, "defuzzifyBlocks", exports.DefuzzifyBlock, false)
   that.ruleBlocks = checkArrayType(d, "ruleBlocks", exports.RuleBlock, false)
   return that
 }
+exports.FunctionBlock.typeName = "FunctionBlock"
