99524165753595aac2ae952306c677bda9df8623
William Blankenship <william.jblankenship@gmail.com>
2014-12-01 16:20:03 -0600

    tmp

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var Transform = require('stream').Transform
 var util = require('util')
+var tokens = require('./constants.js')
 
 var fuzzee = module.exports = function constructor(fsl) {
   Transform.call(this)
-  console.log("test")
+  this.s = ""
   if(!fsl) {
     //We are a stream
   } else{
@@ -13,9 +14,14 @@ var fuzzee = module.exports = function constructor(fsl) {
 util.inherits(fuzzee, Transform);
 fuzzee.prototype._transform = transform
 
-
 function transform(chunk,encoding,cb) {
-  cb(null,chunk)
+  this.s += chunk
+  console.log(tokens)
+  console.log(this.s.replace(/[\n\f\r]+/,' $&').split(/[ \t\v]+/))
+  var symbols = this.s.replace(tokens.NEWLINE,' $&').split(/[ \t\v]+/)
+  this.s = symbols.pop()
+  //console.log(symbols)
+  //cb(null,chunk)
 }
 
 fuzzee.prototype.set = function set(key,val) {
@@ -27,3 +33,5 @@ fuzzee.prototype.get = function get(key) {
 
 fuzzee.prototype.evaluate = function evaluate() {
 }
+
+module.exports = fuzzee
